0b8518f6fed155584707e3a1009b0663a1c1d170
Nora <162056222+Norax4@users.noreply.github.com>
2024-11-30 11:34:11 -0300

    Add files via upload

--- Obligatorio2Web/Datos/AppDbContext.cs
+++ Obligatorio2Web/Datos/AppDbContext.cs
@@ -8,7 +8,6 @@ namespace Obligatorio2Web.Datos
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Habitacion> Habitaciones { get; set; }
-        public DbSet<Persona> Personas { get; set; }
         public DbSet<Huesped> Huespedes { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Reserva> Reservas { get; set; }
